e366221814be9e9bcf5b11d039dec84f69937f95
piedaddy232 <emmanuelsambo011@gmail.com>
2025-01-04 22:37:08 +0100

    li

--- name-service-backend/server.js
+++ name-service-backend/server.js
@@ -14,7 +14,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-    origin: ' http://127.0.0.1:5500 ',
+    origin: ' https://airdaonameservice.vercel.app/ ',
 }));
 app.use(express.json());
 
